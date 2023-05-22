b0c8be1a26857de42dd66381e09cc705ae88f753
Florian NITCHEU <floup221@gmail.com>
2023-05-22 16:25:34 +0200

    unknown steptype everywhere and Library.any method

--- tests/playwright/complexe.spec.ts
+++ tests/playwright/complexe.spec.ts
@@ -1,7 +1,5 @@
-import { Page, test } from "@playwright/test";
+import { test } from "@playwright/test";
 import GherkinWrapper from "../../src";
-import { DataTable as RawDataTable } from "@cucumber/messages";
-import { DataTable } from "@cucumber/cucumber";
 
 
 const wrapper = new GherkinWrapper.forPlaywright(test)
@@ -10,13 +8,10 @@ wrapper.beforeTag('@skip', () => {
     test.skip(true, 'Tagged @skip')
 })
 
-const defaultHandler = async ({page}: {page: Page}, {dataTable, rawdataTable}: {dataTable?: DataTable, rawdataTable?: RawDataTable}) => {
-    if (dataTable) console.log(dataTable.raw(), rawdataTable)
-    await page.waitForTimeout(1000)
-}
 
-wrapper.given(/.*/, defaultHandler)
-wrapper.when(/.*/, defaultHandler)
-//wrapper.then(/.*/, defaultHandler)
+wrapper.any(/.*/, async ({page}, {dataTable}) => {
+    if (dataTable) console.log(dataTable)
+    await page.waitForTimeout(1000)
+})
 
 wrapper.test('./tests/complexe.feature')
